92eebd3b3e50156905531d01f6072029ac387236
王鑫 <wb_wangxin06@kuaishou.com>
2024-01-24 14:30:19 +0800

    全局拦截器

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,14 +9,18 @@ import { AuthModule } from '@/mobile/view/auth/auth.module';
 import { SwaggerModule } from '@/mobile/view/swagger/swagger.module';
 import { UserRolesAccessModule } from '@/mobile/view/userRolesAccess/userRolesAccess.module';
 
-import { User } from '@/typeorm/mysql/user.entity';
-import { AccessEntity } from '@/typeorm/mysql/access.entity';
-import { Posts } from '@/typeorm/mysql/posts.entity';
-import { RoleEntity } from '@/typeorm/mysql/role.entity';
-import { RoleAccess } from '@/typeorm/mysql/roleAccess.entity';
-import { Tags } from '@/typeorm/mysql/tags.entity';
-import { UserExtend } from '@/typeorm/mysql/userExtend.entity';
-import { UserRoles } from '@/typeorm/mysql/userRoles.entity';
+// 拦截器
+import { ResponseInterceptor } from '@/shared/interceptor/response.interceptor';
+
+// 表
+// import { User } from '@/typeorm/mysql/user.entity';
+// import { AccessEntity } from '@/typeorm/mysql/access.entity';
+// import { Posts } from '@/typeorm/mysql/posts.entity';
+// import { RoleEntity } from '@/typeorm/mysql/role.entity';
+// import { RoleAccess } from '@/typeorm/mysql/roleAccess.entity';
+// import { Tags } from '@/typeorm/mysql/tags.entity';
+// import { UserExtend } from '@/typeorm/mysql/userExtend.entity';
+// import { UserRoles } from '@/typeorm/mysql/userRoles.entity';
 
 // @UsePipes(new JwtAuthPipe())
 @Module({
@@ -39,15 +43,15 @@ import { UserRoles } from '@/typeorm/mysql/userRoles.entity';
             charset: "utf8mb4",
             timezone: "local",
             entities: [ // 定义TypeORM需要查找的数据模型的,可以定义多个
-                // __dirname + '/typeorm/mysql/*{.ts,.js}'
-                User,
-                Posts,
-                Tags,
-                UserExtend,
-                RoleAccess,
-                RoleEntity,
-                AccessEntity,
-                UserRoles
+                __dirname + '/typeorm/mysql/*{.ts,.js}'
+                // User,
+                // Posts,
+                // Tags,
+                // UserExtend,
+                // RoleAccess,
+                // RoleEntity,
+                // AccessEntity,
+                // UserRoles
             ],
         }),
         HomeModule,
@@ -58,7 +62,12 @@ import { UserRoles } from '@/typeorm/mysql/userRoles.entity';
         UserRolesAccessModule
     ],
     controllers: [],
-    providers: []
+    providers: [
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: ResponseInterceptor,
+        }
+    ]
 })
 export class AppModule {
 }
--- src/shared/interceptor/response.interceptor.ts
+++ src/shared/interceptor/response.interceptor.ts
@@ -6,16 +6,17 @@ import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 // 返回体结构
-interface Response<T> {
+interface ResponseData<T> {
     data: T;
 }
 
+// 拦截器 每次返回的时候自动带上 code: 200
 @Injectable()
-export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
+export class ResponseInterceptor<T> implements NestInterceptor<T, ResponseData<T>> {
     intercept(
         context: ExecutionContext,
         next: CallHandler<T>,
-    ): Observable<Response<T>> {
+    ): Observable<ResponseData<T>> {
         // 解析 ExecutionContext 的数据内容获取到请求体
         const ctx = context.switchToHttp();
         const request = ctx.getRequest();
@@ -24,6 +25,9 @@ export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
         return next.handle().pipe(
             map(data => {
                 console.log('全局响应拦截器方法返回内容后...');
+                const res: any = context.switchToHttp().getResponse();
+                console.log(res.json({ code: 200, data }));
+
                 return {
                     statusCode: 0,
                     timestamp: new Date().toISOString(),
