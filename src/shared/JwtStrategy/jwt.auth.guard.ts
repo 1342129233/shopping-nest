901644eee392f33a2fa00b0cdeb6f0e153f2ceda
王鑫 <wb_wangxin06@kuaishou.com>
2024-01-25 17:30:45 +0800

    token续期, 互踢, 单点登录, 多端登陆, 限制账号 处理方法

--- src/shared/JwtStrategy/jwt.auth.guard.ts
+++ src/shared/JwtStrategy/jwt.auth.guard.ts
@@ -32,8 +32,18 @@ export class JwtAuthGuard implements CanActivate {
                         // 新的 token, 新的 token 最好获取用户 账户密码去生成
                         const newToken = this.jwtService.sign(validRes);
                         // 设置新的 token 在 redis 中
-                        // const client = this.redisService.getClient();
-                        // await client.set(validRes.sub, newToken, 'EX', 60 * 60 * 24); // 设置过期时间为24小时
+                        // const client = this.redisService.getClient(); // 获取
+
+                        // 如果用户已经登录，则踢掉之前的token，更新token并且存入新的token
+                        // const userTokens = await client.lrange(user.userId, 0, -1); 获取数组
+                        // 如果用户已经登录，则踢掉之前的token，更新token并且存入新的token
+                        // if (userTokens.length >= 3) {
+                        //     await client.lpop(user.userId);
+                        // }
+                        // await client.rpush(user.userId, token);
+
+
+                        // await client.set(username, newToken, 'EX', 60 * 60 * 24); // 设置过期时间为24小时
                         response.setHeader('token', newToken);
                     }
                     return true;
@@ -60,3 +70,10 @@ function windowCookies(cookies: string) {
     }
     return cookieMap.get('token');
 }
+
+// token续期: 获取时间,是否需要生成新的 token
+// 互踢: 重新生成 token
+// 单点登录 sso: 登录验证
+// 多端登陆: 每次用户在新设备上登录时，都会生成新的JWT
+// 限制账号 5个终端 同时登陆: 存入一个 new Map(), 如果用户已经登录，则踢掉之前的token，更新token并且存入新的token
+// 限制ip: 中间件将检查每个请求的IP地址，如果它不在允许的IP地址列表中，就会抛出一个ForbiddenException
